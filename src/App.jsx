dfdbfff22d79b2cf700e8fc1afaee63904f03ea5
Mayli Reyes <ryzmay2001@gmail.com>
2021-04-14 22:40:39 -0500

    Version 1

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+const App = () =>{
+
+    const [counter, setCounter] = useState(0);
+    const [increment, setIncrement] = useState(1);
+
+    const add = () => {
+        setCounter(counter + increment)
+    }
+
+    const subtract = () => {
+        setCounter(counter - increment)
+    }
+
+    const reset = () => {
+        setCounter(0)
+    }
+
+    const addInc = () => {
+        setIncrement(increment + 1)
+    }
+
+    const subtractInc = () => {
+        setIncrement(increment - 1)
+    }
+
+    const resetInc = () => {
+        setIncrement(1)
+    }
+
+    return(
+        <main>
+            <article>
+                <h1>COUNTER</h1>
+                <h2>{counter}</h2>
+                <section className="buttons">
+                    <button onClick={add}>+</button>
+                    <button onClick={subtract}>-</button>
+                    <button onClick={reset}>⭯</button>
+                </section>
+            </article>
+            <article>
+                <h1>INCREMENT</h1>
+                <h2>{increment}</h2>
+                <section className="buttons">
+                    <button onClick={addInc}>+</button>
+                    <button onClick={subtractInc}>-</button>
+                    <button onClick={resetInc}>⭯</button>
+                </section>
+            </article>
+        </main>
+    )
+}
+
+export default App
